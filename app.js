53f7e80012cbb936afe2e6ac3cb7ba26d582cc51
Dario Castiglione <dario.castiglione99@gmail.com>
2021-10-30 18:14:01 +0200

    ..

--- app.js
+++ app.js
@@ -41,22 +41,29 @@ let round = 2;
 let score = 1;
 let winCard = cards[0];
 let roundTime = 300;
-function start(){
-    const starter = setInterval(function(){ change(); }, 300);
+winCard.style.backgroundImage = "url('re_coppe.jpg')";
+function startChange(){ 
     
+    const starter = setInterval(function(){ change(); }, 600);   
     roundTime = roundTime * round;
     setTimeout(() => {clearInterval(starter)}, roundTime);
     console.log(roundTime)
     roundTime = 300;
-
     function win(event){
-        if (event.target === winCard) round = round + 0.5 ;
+        if (event.target === winCard) round = round + 0.5 ,
+        winCard.style.transform = "",
+        winCard.style.backgroundImage = "url('re_coppe.jpg')";
         else round = 2;       
     } 
     console.log(round)
-    
+    winCard.style.transform = ""
     for (x of cards) x.addEventListener("click",win)
+    
 }
-
+const start = () => {
+    startChange()
+    winCard.style.transform = "rotatey(180deg)"
+    winCard.style.backgroundImage = "";
+}
 Btn[0].addEventListener("click",start)   
 
